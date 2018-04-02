27c56405c7de2061f20b4426eb19d785f5e5a3bc
TJ <league@WTS19.attlocal.net>
2018-04-01 17:27:14 -0700

    Finished

--- src/data_structures/TestMatchingBrackets.java
+++ src/data_structures/TestMatchingBrackets.java
@@ -8,35 +8,38 @@ import java.util.Stack;
 import org.junit.Test;
 
 public class TestMatchingBrackets {
-	Stack<Interger> bracketCount = new Stack<Interger>();
+	Stack<Integer> bracketCount = new Stack<Integer>();
 	@Test
 	public void testMatchingBrackets() {
 		assertTrue(doBracketsMatch("{}"));
 		assertTrue(doBracketsMatch("{{}}"));
 		assertTrue(doBracketsMatch("{}{}{{}}"));
-		assertFalse(doBracketsMatch("{{}")s);
+		assertFalse(doBracketsMatch("{{}"));
 		assertFalse(doBracketsMatch("}{"));
 	}
 
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
-	private boolean doBracketsMatch(String b) {
-		return false;
-	}
 	
-	boolean doBracketsMatch(){
-		if(its a open braket) {
-			bracketCount.push(bracket representor);
+	boolean doBracketsMatch(String b){
+		for (int i = 0; i < b.length(); i++) {
+			
+		
+		if(b.charAt(i)=='{') {
+			bracketCount.push(1);
+			
 		}
-		if(its a closed brackt) {
-			bracketCount.pop(brackt represetor);
+		if(b.charAt(i)=='}') {
+			bracketCount.pop();
 			}
-		if(bracketCount.size= 0) {
+		
+		}	
+		if(bracketCount.size()== 0) {
 			return true;
 		}
 		else
 		return false;
+	
+	
+	
 	}
-	
-	
-	
 }
